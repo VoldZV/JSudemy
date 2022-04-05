d6f2d7caac4177d6a0b6aa6aec009e633d1cb149
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2022-04-05 09:43:37 +0300

    Lesson 12.2 c- create object personalMovieDB

--- lessons.js
+++ lessons.js
@@ -2,3 +2,12 @@
 
 let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?','');
 
+//Lesson 12.2
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+
