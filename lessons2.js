03b11c4346385a80faa89aecf3cad61c063803cc
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2022-04-06 11:16:32 +0300

    Lesson 20

---
+++ lessons2.js
@@ -0,0 +1,25 @@
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    color: {
+        border: 'black',
+        bg: 'red'
+    }
+};
+
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options)
+
+for (let key in options) {
+    if(typeof(options[key]) === 'object'){
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`)
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`)
+    }
+}
